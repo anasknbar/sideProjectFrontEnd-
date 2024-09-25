40ce0b2bdd7d1f63497cdd7e68ea06b828000f2c
anasknbar <knbaranasgithub@gmail.com>
2024-09-25 16:40:40 +0000

    setup the sidebar and the header componets

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -1,9 +1,9 @@
 'use client'
 import { useContext, useState } from "react";
-import { AuthContext } from "../context/authContext";
-import UserHeader from "./userHeader";
+import { AuthContext } from "../../context/authContext";
 import GuestHeader from "./guestHeader";
-export default function Header() {
+import UserHeader from "./userHeader";
+export default function Header({setActivePage}) {
     const {tokens,logout} = useContext(AuthContext);
     const [showLoginForm,setShowLoginForm] = useState(false)
 
@@ -17,7 +17,7 @@ export default function Header() {
 
   return (
     <>
-      {!tokens?(<GuestHeader/>):(<UserHeader/>)}
+      {!tokens?(<GuestHeader/>):(<UserHeader setActivePage = {setActivePage}/>)}
       
     </>
   );
--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,24 @@
 import "./globals.css";
 import AuthWrapper from "./context/authContext";
 import { ThemeProvider } from "next-themes";
-import Head from "next/head";
+import { Roboto } from '@next/font/google';
+
+const roboto = Roboto({
+  weight: ['400', '700'], // Choose the font weights you need
+  subsets: ['latin'],     // Choose the character subsets
+});
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head><link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css" rel="stylesheet" />
       </head>
-      <body>
+      <body className={roboto.className}>
         <AuthWrapper>
           <ThemeProvider attribute="class" defaultTheme="light">
           <main>{children}</main>{" "}
           </ThemeProvider>
         </AuthWrapper>
         <script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js"></script>
-
       </body>
     </html>
   );
